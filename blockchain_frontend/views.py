afbb1113178ad915ab2c1bc4d622ba524fbe6835
NicoMalta <niicou96@hotmail.com>
2020-05-04 21:01:57 -0300

    Thesis - Partitioned template

--- blockchain_frontend/views.py
+++ blockchain_frontend/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+
 def prehome(request):
     return render(request, 'prehome.html')
--- medical_story/views.py
+++ medical_story/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request, 'index_medical.html')
+    return render(request, 'medical/index_medical.html')
