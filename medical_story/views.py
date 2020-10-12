a52a9d0bc4abb01a79366bf8e17b3d720e8d338b
TinchoCasazza <martin.casazza1@gmail.com>
2020-10-12 17:41:41 -0300

    Thesis - Added medical history smart contract

--- medical_story/views.py
+++ medical_story/views.py
@@ -47,8 +47,117 @@ def patient_index(request):
 
 
 def history_index(request):
-    return render(request, 'medical/index_history.html')
+    submit_to_blockchain()
+
+    w3 = get_provider()
+    w3.eth.defaultAccount = w3.eth.accounts[0]
+
+    compiled_sol = get_compiled_medical_history()
+
+    abi = json.loads(compiled_sol['contracts']['History.sol']['HistoryMedical']['metadata'])['output']['abi']
+
+    history = w3.eth.contract(
+        address="0x9f30617b9dAA71481E9B7ECB1F26E8Cb7d1B9640",
+        abi=abi
+    )
+
+    histories = history.functions.getByPatient(1).call()
+
+    return render(request, 'medical/index_history.html', {'histories': histories})
 
 
 def profile_index(request):
     return render(request, 'medical/index_profile.html')
+
+
+def get_compiled_medical_history():
+    return compile_standard({
+
+        "language": "Solidity",
+        "sources": {
+            "History.sol": {
+                "content": '''
+                    pragma solidity >=0.0.0;
+                    pragma experimental ABIEncoderV2;
+                    
+                    contract HistoryMedical {
+                        
+                        struct History {
+                            int  patient;
+                            int  doctor;
+                            string  diagnostic;
+                            string  place;
+                            string  description;
+                            string  receta;
+                        }
+                        
+                        
+                        History[] public submittedHistory;
+                        mapping (int => History[]) public historyByPatient;
+                    
+                    
+                        function submitHistory( int  patient,  int  doctor, string memory diagnostic,  string memory place, string memory description, string memory receta) public {
+                            History memory history = History(patient, doctor, diagnostic, place, description, receta);    
+                            
+                            submittedHistory.push(history);
+                            
+                            History[] storage allHistory = historyByPatient[patient];
+                            allHistory.push(history);
+                            
+                            historyByPatient[patient] = allHistory;
+                        }
+                        
+                        
+                        function getSubmittedHistory() public view returns (History[] memory) {
+                            return submittedHistory;
+                        }
+                        
+                    
+                        function getByPatient(int patient) public returns (History[] memory){
+                            return historyByPatient[patient];
+                        }
+                    }
+
+                   '''
+            }
+        },
+        "settings":
+            {
+                "outputSelection": {
+
+                    "*": {
+
+                        "*": [
+                            "metadata", "evm.bytecode"
+                            , "evm.bytecode.sourceMap"
+                        ]
+                    }
+                }
+            }
+    })
+
+
+def get_provider():
+    my_provider = Web3.HTTPProvider('http://localhost:8545')
+    return Web3(my_provider)
+
+
+def submit_to_blockchain():
+    w3 = get_provider()
+    w3.eth.defaultAccount = w3.eth.accounts[0]
+
+    compiled_sol = get_compiled_medical_history()
+
+    abi = json.loads(compiled_sol['contracts']['History.sol']['HistoryMedical']['metadata'])['output']['abi']
+
+    history = w3.eth.contract(
+        address="0x9f30617b9dAA71481E9B7ECB1F26E8Cb7d1B9640",
+        abi=abi
+    )
+
+    #history.functions.submitHistory(2, 123, "probando", "probando", "probando", "probando").transact()
+
+    #print(history.functions.getSubmittedHistory().call())
+
+    #print(history.functions.getByPatient(1).call())
+
